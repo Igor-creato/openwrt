3f2f1bc0c30adec42485f7c3590c854ace51a535
Igor-creato <71209783+Igor-creato@users.noreply.github.com>
2025-11-05 10:43:51 +0300

    Update add-domain.sh

--- add-domain.sh
+++ add-domain.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+# Запрос домена у пользователя
 echo "Введите домен для добавления:"
 read DOMAIN
 
+# Проверка, есть ли домен уже в списке
 if uci get dhcp.@ipset[0].domain | grep -qw "$DOMAIN"; then
   echo "Домен уже добавлен!"
 else
+  # Добавляем домен в конфигурацию
   uci add_list dhcp.@ipset[0].domain="$DOMAIN"
   uci commit dhcp
   echo "Домен добавлен: $DOMAIN"
 fi
 
+# Перезапускаем dnsmasq и firewall
 /etc/init.d/dnsmasq restart
 /etc/init.d/firewall restart
 
